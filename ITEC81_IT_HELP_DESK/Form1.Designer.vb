a07c654bb25a3710d7a2eba7f6d34ce7b1d74393
Verwin Polloso <verwinpolloso2@gmail.com>
2025-12-07 18:34:41 +0800

    asdasdas

--- ITEC81_IT_HELP_DESK/Form1.Designer.vb
+++ ITEC81_IT_HELP_DESK/Form1.Designer.vb
@@ -4,7 +4,7 @@ Partial Class Form1
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()>
-    Protected Overrides Sub Dispose(disposing As Boolean)
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
                 components.Dispose()
@@ -18,92 +18,113 @@ Partial Class Form1
     Private components As System.ComponentModel.IContainer
 
     'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.
+    'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Label1 = New Label()
-        username_tb = New TextBox()
-        password_tb = New TextBox()
-        Label2 = New Label()
-        btn_login = New Button()
-        btn_signin = New Button()
+        lblTitle = New Label()
+        lblUser = New Label()
+        txtUsername = New TextBox()
+        lblPass = New Label()
+        txtPassword = New TextBox()
+        btnLogin = New Button()
+        btnExit = New Button()
         SuspendLayout()
         ' 
-        ' Label1
+        ' lblTitle
         ' 
-        Label1.AutoSize = True
-        Label1.Location = New Point(239, 149)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(75, 20)
-        Label1.TabIndex = 0
-        Label1.Text = "Username"
+        lblTitle.AutoSize = True
+        lblTitle.Font = New Font("Segoe UI", 14.0F, FontStyle.Bold)
+        lblTitle.Location = New Point(149, 30)
+        lblTitle.Margin = New Padding(4, 0, 4, 0)
+        lblTitle.Name = "lblTitle"
+        lblTitle.Size = New Size(78, 32)
+        lblTitle.TabIndex = 0
+        lblTitle.Text = "Login"
         ' 
-        ' username_tb
+        ' lblUser
         ' 
-        username_tb.Location = New Point(320, 146)
-        username_tb.Name = "username_tb"
-        username_tb.Size = New Size(125, 27)
-        username_tb.TabIndex = 1
+        lblUser.AutoSize = True
+        lblUser.Location = New Point(53, 108)
+        lblUser.Margin = New Padding(4, 0, 4, 0)
+        lblUser.Name = "lblUser"
+        lblUser.Size = New Size(78, 20)
+        lblUser.TabIndex = 1
+        lblUser.Text = "Username:"
         ' 
-        ' password_tb
+        ' txtUsername
         ' 
-        password_tb.Location = New Point(320, 179)
-        password_tb.Name = "password_tb"
-        password_tb.PasswordChar = "*"c
-        password_tb.Size = New Size(125, 27)
-        password_tb.TabIndex = 3
+        txtUsername.Location = New Point(57, 132)
+        txtUsername.Margin = New Padding(4, 5, 4, 5)
+        txtUsername.Name = "txtUsername"
+        txtUsername.Size = New Size(265, 27)
+        txtUsername.TabIndex = 2
         ' 
-        ' Label2
+        ' lblPass
         ' 
-        Label2.AutoSize = True
-        Label2.Location = New Point(239, 182)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(70, 20)
-        Label2.TabIndex = 2
-        Label2.Text = "Password"
+        lblPass.AutoSize = True
+        lblPass.Location = New Point(53, 185)
+        lblPass.Margin = New Padding(4, 0, 4, 0)
+        lblPass.Name = "lblPass"
+        lblPass.Size = New Size(73, 20)
+        lblPass.TabIndex = 3
+        lblPass.Text = "Password:"
         ' 
-        ' btn_login
+        ' txtPassword
         ' 
-        btn_login.Location = New Point(333, 212)
-        btn_login.Name = "btn_login"
-        btn_login.Size = New Size(94, 29)
-        btn_login.TabIndex = 4
-        btn_login.Text = "Log In"
-        btn_login.UseVisualStyleBackColor = True
+        txtPassword.Location = New Point(57, 209)
+        txtPassword.Margin = New Padding(4, 5, 4, 5)
+        txtPassword.Name = "txtPassword"
+        txtPassword.PasswordChar = "*"c
+        txtPassword.Size = New Size(265, 27)
+        txtPassword.TabIndex = 4
         ' 
-        ' btn_signin
+        ' btnLogin
         ' 
-        btn_signin.Location = New Point(333, 247)
-        btn_signin.Name = "btn_signin"
-        btn_signin.Size = New Size(94, 29)
-        btn_signin.TabIndex = 5
-        btn_signin.Text = "Sign In"
-        btn_signin.UseVisualStyleBackColor = True
+        btnLogin.Location = New Point(57, 277)
+        btnLogin.Margin = New Padding(4, 5, 4, 5)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(120, 46)
+        btnLogin.TabIndex = 5
+        btnLogin.Text = "Login"
+        btnLogin.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(204, 277)
+        btnExit.Margin = New Padding(4, 5, 4, 5)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(120, 46)
+        btnExit.TabIndex = 6
+        btnExit.Text = "Exit"
+        btnExit.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Controls.Add(btn_signin)
-        Controls.Add(btn_login)
-        Controls.Add(password_tb)
-        Controls.Add(Label2)
-        Controls.Add(username_tb)
-        Controls.Add(Label1)
+        ClientSize = New Size(379, 402)
+        Controls.Add(btnExit)
+        Controls.Add(btnLogin)
+        Controls.Add(txtPassword)
+        Controls.Add(lblPass)
+        Controls.Add(txtUsername)
+        Controls.Add(lblUser)
+        Controls.Add(lblTitle)
+        Margin = New Padding(4, 5, 4, 5)
         Name = "Form1"
         StartPosition = FormStartPosition.CenterScreen
-        Text = "Form1"
+        Text = "IT Help Desk - Login"
         ResumeLayout(False)
         PerformLayout()
+
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents username_tb As TextBox
-    Friend WithEvents password_tb As TextBox
-    Friend WithEvents Label2 As Label
-    Friend WithEvents btn_login As Button
-    Friend WithEvents btn_signin As Button
-
+    Friend WithEvents lblTitle As System.Windows.Forms.Label
+    Friend WithEvents lblUser As System.Windows.Forms.Label
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents lblPass As System.Windows.Forms.Label
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
 End Class
